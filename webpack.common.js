72e1b238a039d64cc2de4c1d50b7c20095dd707b
jeetsil07 <jeetsil100@gmail.com>
2024-12-02 12:26:52 +0530

    add automateion

--- webpack.common.js
+++ webpack.common.js
@@ -4,56 +4,59 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = (env) => ({
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    chunkFilename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name].[contenthash][ext]",
+module.exports = (env) => {
+  // Determine the correct .env file based on the ENVIRONMENT
+  const dotenvPath = env.ENVIRONMENT === "dev" ? './.env' : `./.env.${env.ENVIRONMENT}`;
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      chunkFilename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
         },
-      },
+        {
+          test: /\.scss$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[name].[contenthash][ext]",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+        inject: true,
+        publicPath: "/",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      }),
+      new Dotenv({
+        path: dotenvPath, // Use the determined path
+        systemvars: true, // Load system environment variables as well
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-      inject: true,
-      publicPath: "/",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    // Load .env file
-    new Dotenv({
-      path: `./.env.${env.ENVIRONMENT || ''}`, // Load .env file
-      systemvars: true, // Load system environment variables as well
-    }),
-  ],
-});
+  };
+};
